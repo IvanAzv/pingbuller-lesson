d4b918a427bd59897b26ecd37ac50ca15b63901e
IvanAzv <ivan.azvolinskiy@gmail.com>
2022-07-25 22:55:08 +0300

    sasassa

--- pingbuller.js
+++ pingbuller.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-      $('.slider').bxSlider();
+  $('.slider').bxSlider();
+});
+    
+$(document).ready(function(){
+  $('.slider_2').bxSlider({
+    speed:100,
+    infiniteLoop:true,
+    auto:true,
+    pager:false,
+    controls: false,
+    minSlides: 2,
+    maxSlides:3,
+  
+      });
     });
